e82ff79649d1ff3879e9529baf874891a97e1d46
Sachinnaik448 <117216236+Sachinnaik448@users.noreply.github.com>
2022-11-30 23:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("home").style.display = "block";
+document.getElementById("service").style.display = "none";
+document.getElementById("about").style.display = "none";
+document.getElementById("contact").style.display = "none";
+
+
+function homePage() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("service").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    
+}
+
+function service() {
+    document.getElementById("service").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+   
+}
+function about() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("service").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+}
+function contact(){
+    document.getElementById("contact").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("service").style.display = "none";
+    document.getElementById("about").style.display = "none";
+}
